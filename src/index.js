728f59112ebca826bd5a8d5c6d2936bb54eb8ef4
zhangzhijie <zhangzhijie@zjdyiot.com>
2018-10-29 16:59:04 +0800

    😀

--- src/index.js
+++ src/index.js
@@ -1,68 +1,74 @@
-var quene = {
-	list: [],
-	stop: false,
-	running: false,
-	presentation: 0,
+(function () {
+  function setTimeIntervalQueue() {
+    this.list = [];
+    this.stop = false;
+    this.running = false;
+    this.presentation = 0;
+  }
 
-	add: function (fn, delay) {
-		if( fn instanceof Array && delay instanceof Array){
-			if(fn.length < delay.length){
-				console.log("You have one or more fn haven't got delay time.");
-				return
-			}
-			for(var i = 0; i < fn.length ; i++){
-				this.list.push({fn: fn[i], delay: delay[i]})
-			}
-		}else if( typeof fn === "function"){
-			this.list.push({fn: fn, delay: delay})
-		}
-	},
+  setTimeIntervalQueue.prototype.add =  function (fn, delay) {
+    if (fn instanceof Array && delay instanceof Array) {
+      if (fn.length < delay.length) {
+        console.log("You have one or more fn haven't got delay time.");
+        return
+      }
+      for (var i = 0; i < fn.length; i++) {
+        this.list.push({fn: fn[i], delay: delay[i]})
+      }
+    } else if (typeof fn === "function") {
+      this.list.push({fn: fn, delay: delay})
+    }
+  };
 
-	clear: function () {
-		this.list = [];
-		this.reset();
-	},
+  setTimeIntervalQueue.prototype.clear = function () {
+    this.list = [];
+    this.reset();
+  };
 
-	run: function () {
-		var _this = this;
+  setTimeIntervalQueue.prototype.continue = function () {
+    if (this.running) {
+      return
+    }
+    this.stop = false;
+    this.run();
+  };
 
-		if (_this.running) {
-			console.log("运行中...");
-			return
-		}
-		_this.running = true;
-		var list = _this.list;
-		var presentation = _this.presentation;
+  setTimeIntervalQueue.prototype.run = function () {
+    var _this = this;
 
-		if (_this.presentation < list.length) {
-			this.timeout = setTimeout(function () {
-				if (!_this.stop) {
-					list[presentation].fn();
-					_this.running = false;
-					_this.presentation++;
-					_this.run();
-				}
-			}, list[presentation].delay)
-		} else {
-			_this.reset()
-		}
-	},
+    if (_this.running) {
+      console.log("运行中...");
+      return
+    }
+    _this.running = true;
+    var list = _this.list;
+    var presentation = _this.presentation;
 
-	reset: function () {
-		this.presentation = 0;
-		this.pause();
-	},
+    if (_this.presentation < list.length) {
+      this.timeout = setTimeout(function () {
+        if (!_this.stop) {
+          list[presentation].fn();
+          _this.running = false;
+          _this.presentation++;
+          _this.run();
+        }
+      }, list[presentation].delay)
+    } else {
+      _this.reset()
+    }
+  };
 
-	pause: function () {
-		this.stop = true;
-		this.running = false;
-		clearTimeout(this.timeout);
-	},
-	continue: function () {
-		if (this.running) {
-			return
-		}
-		this.stop = false;
-		this.run();
-	}
-};
+  setTimeIntervalQueue.prototype.reset = function () {
+    this.presentation = 0;
+    this.pause();
+  };
+
+  setTimeIntervalQueue.prototype.pause = function () {
+    this.stop = true;
+    this.running = false;
+    clearTimeout(this.timeout);
+  };
+
+  window.setTimeIntervalQueue = setTimeIntervalQueue;
+
+})(window);
